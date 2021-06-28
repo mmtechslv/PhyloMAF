c1132dae7a778811a6abd9bafd329b6067916396
mmtechslv <mmtechslv@gmail.com>
2021-06-28 11:30:55 +0400

    Setup.py and ext adjustments

--- setup.py
+++ setup.py
@@ -1,4 +1,27 @@
 from setuptools import setup
+from Cython.Build import cythonize
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+cython_root = "internal/_extensions/_cython/"
+cpython_root = "internal/_extensions/_cpython/"
+
+ext_modules = [
+    cythonize(
+        cython_root + "_source/cython_functions.pyx",
+        compiler_directives={"language_level": "3"},
+    ),
+    Extension(
+        "PMAFCEXT",
+        sources=[
+            cpython_root + "_pmafc_extension/_source/c_parser.c",
+            cpython_root + "_pmafc_extension/_source/c_hmerizer.c",
+        ],
+        include_dirs=[cpython_root + "_pmafc_extension/_source"],
+    ),
+]
+
+cmdclass = {"build_ext": build_ext}
 
 setup(
     name="PhyloMAF",
@@ -17,6 +40,22 @@ setup(
     license="BSD-3-Clause",
     author="Farid Musa",
     author_email="farid.musa.h@gmail.com",
+    install_requires=[
+        "execnet",
+        "biom-format",
+        "Cython",
+        "fuzzywuzzy",
+        "pandas",
+        "dateparser",
+        "numpy",
+        "pytest",
+        "urllib3",
+        "ete3",
+        "pytables",
+        "biopython",
+        "scikit-bio",
+    ],
+    ext_modules=ext_modules,
+    cmdclass=cmdclass,
     description="Phylogenetic Microbiome Analysis Framework",
 )
-## Cython and Cpython extension must be compiled
