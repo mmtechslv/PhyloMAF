4a663aef79f76ef10075bc3facc68f79bea1b961
mmtechslv <mmtechslv@gmail.com>
2021-06-28 20:35:18 +0400

    TravisCI, RTD, dependencies fixes

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup, find_packages
 from Cython.Build import cythonize
 from distutils.extension import Extension
+# from setuptools.command.build_ext import build_ext
 from setuptools.command.build_ext import build_ext as _build_ext
+
 import os
 
 USE_CYTHON = os.environ.get("USE_CYTHON", False)
@@ -14,7 +16,6 @@ cpython_root = "pmaf/internal/_extensions/_cpython/"
 class build_ext(_build_ext):
     def finalize_options(self):
         super().finalize_options()
-        build_py = self.get_finalized_command("build_py")
         self.inplace = True
 
 
@@ -27,17 +28,29 @@ ext_modules = [
         ],
         include_dirs=[cpython_root + "_pmafc_extension/_source"],
     ),
-] + cythonize(
     Extension(
         "pmaf.internal._extensions.cython_functions",
         [cython_root + "_source/cython_functions" + ext],
     ),
-    compiler_directives={"language_level": "3"},
-)
+]
 
 
-print(ext_modules)
-cmdclass = {"build_ext": build_ext}
+# ext_modules = [
+#     Extension(
+#         "pmaf.internal._extensions.PMAFCEXT",
+#         sources=[
+#             cpython_root + "_pmafc_extension/_source/c_parser.c",
+#             cpython_root + "_pmafc_extension/_source/c_hmerizer.c",
+#         ],
+#         include_dirs=[cpython_root + "_pmafc_extension/_source"],
+#     ),
+# ] + cythonize(
+#     Extension(
+#         "pmaf.internal._extensions.cython_functions",
+#         [cython_root + "_source/cython_functions" + ext],
+#     ),
+#     compiler_directives={"language_level": "3"},
+# )
 
 setup(
     name="PhyloMAF",
@@ -62,7 +75,8 @@ setup(
         "biopython",
         "scikit-bio",
     ],
-    ext_modules=ext_modules,
-    cmdclass=cmdclass,
+    ext_modules=cythonize(ext_modules),
+    compiler_directives={"language_level": "3"},
+    cmdclass={"build_ext": build_ext},
     description="Phylogenetic Microbiome Analysis Framework",
 )
